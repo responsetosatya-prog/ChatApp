542cee18fee3e53046678542b8ec70eddb208f13
responsetosatya-prog <responsetosatya@gmail.com>
2026-06-28 02:13:07 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Chat from "./pages/Chat";
+
+function App() {
+
+    return (
+
+        <BrowserRouter>
+
+            <Routes>
+
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Chat />} />
+
+            </Routes>
+
+        </BrowserRouter>
+
+    );
+
+}
+
+export default App;
